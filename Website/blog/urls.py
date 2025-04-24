cac36e0d6ed4254049ccf1d92251def6a8cf4905
Anosh <anosh2@Anosh2s-MBP.attlocal.net>
2025-04-24 18:27:17 -0500

    Added imgages and linked pages

--- Website/blog/urls.py
+++ Website/blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
-    path("posts/", views.posts, name="posts"),
+    path("posts/", views.posts, name="all-posts"),
     path("posts/<slug:slug>", views.post_details, name="post-details") #posts/my-first-post
 ]
